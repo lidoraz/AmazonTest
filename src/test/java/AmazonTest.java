6cceb3820e26ef7230c5af633104628b6c3d2e51
lidoraz <lidor.az@gmail.com>
2017-11-02 11:20:36 +0200

    added str test

---
+++ src/test/java/AmazonTest.java
@@ -0,0 +1,9 @@
+public class AmazonTest {
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+
+    public static String getStr(){
+        return "hi";
+    }
+}
