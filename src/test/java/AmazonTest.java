c8d0d070b7475697ad6ef72eddb8026779e9b634
lidoraz <lidor.az@gmail.com>
2017-11-02 12:05:02 +0200

    added str test

--- src/test/java/AmazonTest.java
+++ src/test/java/AmazonTest.java
@@ -1,6 +1,14 @@
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.Bucket;
+
 public class AmazonTest {
     public static void main(String[] args){
         System.out.println("hello world");
+
+        AmazonS3Client a=new AmazonS3Client();
+        Bucket alexa=a.createBucket("alexa");
+
+        System.out.println(alexa.getName());
     }
 
     public static String getStr(){
